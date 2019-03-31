ba2e5d2ac03073efdb3b16ef46b4d88041e9034a
aparmentier <a.g.parmentier@gmail.com>
2019-03-31 10:46:55 -0400

    Fixed: main is easier to use now.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,10 @@ from mongo_query import load_comments
 from trees import build_trees
 
 
-def interactive():
+def reload():
     comments = load_comments()
     trees = build_trees(comments)
-    return comments, trees
+    nonempty_trees = [(c, stf) for c,stf in trees if c.children]
+    return comments, trees, nonempty_trees
 
-
-comments, trees = interactive()
-nonempty_trees = [(c, stf) for c,stf in trees if c.children]
+comments, trees, nonempty_trees = reload()
